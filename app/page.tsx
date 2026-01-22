d07f68d16544bfd076dd76c44d00fd6957fd638f
liammfrenkel-gif <liammfrenkel@gmail.com>
2026-01-22 16:58:36 -0500

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>Gift Quiz 🎁</h1>
-      <p>This is working.</p>
-      <a href="/results">Go to results</a>
-    </main>
-  );
-}
