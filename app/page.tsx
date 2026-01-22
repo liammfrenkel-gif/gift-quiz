be8cbcdf82299d2db640f512807e3259735959e1
liammfrenkel-gif <liammfrenkel@gmail.com>
2026-01-22 17:04:27 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Gift Quiz 🎁</h1>
+      <p>The homepage is alive.</p>
+      <a href="/results">Go to results</a>
+    </main>
+  );
+}
